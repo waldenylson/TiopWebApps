93bc742234496329a5e0e4f438b410f7f1e1df11
Waldenylson Silva <waldenylson.silva@yahoo.com.br>
2018-04-16 07:35:22 -0300

    Não Lembro, mas ta aí...

--- app/Units/Dashboard/Resources/Views/sinoptico.blade.php
+++ app/Units/Dashboard/Resources/Views/sinoptico.blade.php
@@ -2,27 +2,34 @@
 {{--{{ dd($radares) }}--}}
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="project_sort">
-                <div id="paninel">
-                    <ul class="project_list">
-                        @foreach($radares as $radar)
-                            <li>
-                                <span class="project_badge {{ $radar['status'] }} iconsweet"></span>
-                                <a class="project_title"><b>{{ $radar['nome'] }}</b></a>
-                                <br>
-                                <a class="project_title">TA-10SST </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="row">
-            <div class="bottom-container">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium adipisci animi at consequuntur explicabo iure nihil, perspiciatis repellat reprehenderit? Assumenda dignissimos est ipsum iure rerum? Incidunt possimus quas sit.
-            </div>
-        </div>
+        {{--<div class="row box float-left">--}}
+            {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eum nam sint voluptates! Assumenda eos est maxime nemo nostrum odio optio quas quidem rerum! Atque facilis iure laborum nisi repudiandae.--}}
+        {{--</div>--}}
+        {{--<div class="float-right">--}}
+            {{--<div>--}}
+                {{--<div class="project_sort">--}}
+                    {{--<div id="paninel">--}}
+                        {{--<ul class="project_list">--}}
+                            {{--@foreach($radares as $radar)--}}
+                                {{--<li>--}}
+                                    {{--<span class="project_badge {{ $radar['status'] }} iconsweet"></span>--}}
+                                    {{--<a class="project_title"><b>{{ $radar['nome'] }}</b></a>--}}
+                                    {{--<br>--}}
+                                    {{--<a class="project_title">TA-10SST </a>--}}
+                                {{--</li>--}}
+                            {{--@endforeach--}}
+                        {{--</ul>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<br>--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<div class="bottom-container">--}}
+                {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium adipisci animi at consequuntur explicabo iure nihil, perspiciatis repellat reprehenderit? Assumenda dignissimos est ipsum iure rerum? Incidunt possimus quas sit.--}}
+            {{--</div>--}}
+        {{--</div>--}}
     </div>
+
+
 @stop
