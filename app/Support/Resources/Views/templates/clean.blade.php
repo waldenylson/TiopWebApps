7c2abeec63941c92428e358fe44ef15772c4d100
Waldenylson Silva <waldenylson.silva@yahoo.com.br>
2018-04-17 00:00:22 -0300

    Design +/-

--- app/Support/Resources/Views/templates/clean.blade.php
+++ app/Support/Resources/Views/templates/clean.blade.php
@@ -6,8 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>  @yield('title') </title>
+    <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
 </head>
 <body>
 @yield('content')
+
+<script src="{!! asset('js/app.js') !!}" type="text/javascript"></script>
 </body>
 </html>
--- app/Units/Dashboard/Resources/Views/sinoptico.blade.php
+++ app/Units/Dashboard/Resources/Views/sinoptico.blade.php
@@ -1,51 +1,107 @@
-@extends('support::templates.fancyboxContent')
+@extends('support::templates.clean')
 {{--{{ dd($radares) }}--}}
 @section('content')
-    {{--<div class="container">--}}
-        {{--<div class="row box float-left">--}}
-            {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eum nam sint voluptates! Assumenda eos est maxime nemo nostrum odio optio quas quidem rerum! Atque facilis iure laborum nisi repudiandae.--}}
-        {{--</div>--}}
-        {{--<div class="float-right">--}}
-            {{--<div>--}}
-                {{--<div class="project_sort">--}}
-                    {{--<div id="paninel">--}}
-                        {{--<ul class="project_list">--}}
-                            {{--@foreach($radares as $radar)--}}
-                                {{--<li>--}}
-                                    {{--<span class="project_badge {{ $radar['status'] }} iconsweet"></span>--}}
-                                    {{--<a class="project_title"><b>{{ $radar['nome'] }}</b></a>--}}
-                                    {{--<br>--}}
-                                    {{--<a class="project_title">TA-10SST </a>--}}
-                                {{--</li>--}}
-                            {{--@endforeach--}}
-                        {{--</ul>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--<br>--}}
-        {{--</div>--}}
-        {{--<div>--}}
-            {{--<div class="bottom-container">--}}
-                {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium adipisci animi at consequuntur explicabo iure nihil, perspiciatis repellat reprehenderit? Assumenda dignissimos est ipsum iure rerum? Incidunt possimus quas sit.--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
 
     <div class="container">
-        <div class="tile is-ancestor">
-            <div class="tile is- is-vertical is-parent">
-                <div class="tile is-child box">
-                    <p class="title">SOBREAVISO</p>
-                </div>
-                <div class="tile is-child box">
-                    <p class="title">Two</p>
+        <div class="sub-container-left">
+            <div class="sobreaviso">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title"><b>Sobreaviso Técnico - Agenda</b></h3>
+                    </div>
+                    <div class="panel-body">
+                        <b>3S SIN WALDENYLSON - 6415</b>
+                    </div>
                 </div>
             </div>
-            <div class="tile is-parent">
-                <div class="tile is-child box">
-                    <p class="title">Three</p>
+
+            <div class="afastamentos">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title"><b>Afastamentos Efetivo</b></h3>
+                    </div>
+                    <div class="panel-body">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam amet asperiores at eaque enim est excepturi facere hic illum in, mollitia necessitatibus perspiciatis recusandae, voluptas voluptatum. Perferendis, voluptatibus?
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sub-container-right">
+            <div class="sinoptico">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title"><b>SINÓPTICO RADARES - REAL TIME</b></h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="float-right">
+                            <div>
+                                <div class="project_sort">
+                                    <div id="panel">
+                                        <ul class="project_list">
+                                            @foreach($radares as $radar)
+                                                <li>
+                                                    <span class="project_badge {{ $radar['status'] }} iconsweet"></span>
+                                                    <a class="project_title"><b>{{ $radar['nome'] }}</b></a>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-container">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci deserunt eius illum nisi quia. A corporis, cupiditate deserunt dolores eveniet magni natus reiciendis similique unde voluptas. Esse ipsum molestiae quod!
                 </div>
             </div>
         </div>
     </div>
+
+    <style>
+        .container {
+            margin: 0;
+            padding: 0;
+            width: 99%;
+            margin: 5px 5px 5px 5px;
+        }
+
+        .sub-container-left {
+            padding-left: 10px;
+            padding-top: 10px;
+            float: left;
+            width: 22%;
+        }
+
+        .sub-container-right{
+            float: left;
+            width: 78%;
+        }
+
+        .sinoptico {
+            float: left;
+            padding-left: 10px;
+            padding-top: 20px;
+        }
+
+        .sobreaviso {
+            padding-top: 10px;
+        }
+
+        .afastamentos {
+            overflow: hidden;
+            height: 100%;
+        }
+
+        .bottom-container {
+            width: 100%;
+            padding-left: 10px;
+            padding-top: 10px;
+            float: left;
+        }
+    </style>
 @stop
