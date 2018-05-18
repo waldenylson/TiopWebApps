2a524f9d50f6b43a6c3289262d314523a7ef8edf
3S SIN WALDENYLSON <waldenylsonwpss@fab.mil.br>
2018-05-18 16:20:14 -0300

    ...

--- app/Units/BDS/Resources/Views/partials/form.blade.php
+++ app/Units/BDS/Resources/Views/partials/form.blade.php
@@ -11,9 +11,19 @@
 <div class="form-group well">
     <div class="row">
         <div class="col-md-2">
-            {!! Form::label('sistema_centro_ida', 'Nome', ['class' => 'control-label']) !!}
+            {!! Form::label('sistema_centro_id', 'Nome', ['class' => 'control-label']) !!}
             {!! Form::text('nome', null, ['class' => 'form-control', 'id' => 'nome', 'autofocus' => '']) !!}
         </div>
+
+        <div class="col-md-2">
+            {!! Form::label('natureza_missao_id', 'Natureza Missão', ['class' => 'control-label']) !!}
+            {!! !empty($requisicao) ? Form::select('natureza_missao_id', $naturezas, $requisicao->natureza_missao_id,
+                                            ['class' => 'form-control', 'id' => 'natureza_missao_id'])
+                                    : Form::select('natureza_missao_id', $naturezas, null,
+                                            ['class' => 'form-control', 'id' => 'natureza_missao_id'])
+            !!}
+        </div>
+
         <div class="col-md-2">
             {!! Form::label('acronimo', 'Acrônimo', ['class' => 'control-label']) !!}
             {!! Form::text('acronimo', null, ['class' => 'form-control', 'id' => 'acronimo']) !!}
