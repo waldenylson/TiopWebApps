02714a621a0bc16802d75bff3fc7e1c96b15b357
3S SIN WALDENYLSON <waldenylsonwpss@fab.mil.br>
2018-06-19 15:16:05 -0300

    ajustando API

--- app/Domains/Radars/Contracts/ApiRadarRepository.php
+++ app/Domains/Radars/Contracts/ApiRadarRepository.php
@@ -1,15 +1,11 @@
 <?php namespace TIOp\Domains\Radars\Contracts;
 
 use Artesaos\Warehouse\Contracts\Segregated\CrudRepository;
-use TIOp\Units\Radars\Requests\StoreRadarsPostRequest;
 
 /**
  *  User Interface Contracts
  */
 interface ApiRadarRepository extends CrudRepository
 {
-    public function updateStatusRadar()
-    {
-        
-    }
+    public function updateStatusRadar($radar, $canal_a, $canal_b);
 }
